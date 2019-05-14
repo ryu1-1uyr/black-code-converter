2a8047b418be5b8c03d87f41e7ee47450bd6bbfd
s17001 <s17001@std.it-college.ac.jp>
2019-05-14 11:41:21 +0900

    パースして置き換える関数の生成

--- main.js
+++ main.js
@@ -1,17 +1,39 @@
 (stdin => {
-    // Define Function
-    const range = (len, start) =>
-      Array.from(Array(len), (v, i) => i + start)
-  
-    const uniq = array => [...new Set(array)]
-  
+    const range = (len, start) => Array.from(Array(len), (v, i) => i + start)
+    const uniq = array => [...new Set(array)] // in array // out array
+
+    const replaceBlackCode = (string) => { // in 1 range string // out some range string 
+        let rebuild = string
+        .replace("t","(!![]+[])[-[]]")
+        .replace("r","(!![]+[])[-~[]]")
+        .replace("u","([]['']+[])[-[]]")
+        .replace("e","(!![]+[])[-~-~[-~[]]]")
+        .replace("f","(![]+[])[-[]]")
+        .replace("a","(![]+[])[-~[]]")
+        .replace("l","(![]+[])[-~[-~[]]]")
+        .replace("s","(![]+[])[-~[-~[-~[]]]]")
+        .replace("n","([]['']+[])[-~[]]")
+        .replace("d","([]['']+[])[-~[-~[]]]")
+        .replace("i","([]['']+[])[(-~[]<<-~[-~[]])+-~[]]")
+        .replace("I","(~[]/-[]+[])[-[]]")
+        .replace("y","(- -~[]/-[]+[])[(-~[]+[]+-~[]>>-~[])+(-~[]<<-~[])]")
+        .replace("o","([...{}+{}])[-~[]]")
+        .replace("b","([...{}+{}])[-~[]<<-~[]]")
+        .replace("j","([...{}+{}])[-~-~[-~[]]]")
+        .replace("c","([...{}+{}])[(-~[]+[]+-~[]>>-~[])]")
+        .replace(' ',' ')
+        return rebuild
+    }
+
+    const canUseString = ["t", "r", "u", "e", "f", "a", "l", "s", "n", "d", "i", "I", "y", "[", "o", "b", "j", "c", " ", "O", "]", "N"]
+
     // Declare Variable
     const inputs  = stdin.toString().trim().split('');
   
     // Main Procedure
-    //["t", "r", "u", "e", "f", "a", "l", "s", "n", "d", "i", "I", "y", "[", "o", "b", "j", "c", " ", "O", "]", "N"]
-    //使用可能文字列
-  
-    console.log(inputs)
-  
+
+    // inputs.map( x => replaceBlackCode(x))    
+
+    console.log(inputs.map( x => replaceBlackCode(x)).join('+'))
+  console.log(inputs)
   })(require('fs').readFileSync('/dev/stdin', 'utf8'));
