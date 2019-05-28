16dba2209b85f747d26266382da942c136743e25
s17001 <s17001@std.it-college.ac.jp>
2019-05-28 12:11:27 +0900

    例外文字のためのテンプレコードを用意した

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 (stdin => {
+    const baseBlackcode =`__=-~-~[-~[]];___=(!![]+[])[-~[]]+(!![]+[])[__]+(!![]+[])[+[]]+(!![]+[])[-~[-~[]]]+(!![]+[])[-~[]]+([][""]+[])[-~[]]+' \"'+'\\' +([][""]+[])[[]-[]] +'{' +(-~[]+[]) +(![]+[])[[]-[]] +__*__ +-~[] +-~[__] +'}' +'\"';_=[...{}+[]][__+__- -~[]]+[...{}+[]][-~[]]+([][""]+[])[-~[]]+(([]==[])+[])[__]+(-~[]/[]+[])[__+__]+(!![]+[])[-~[]]+([][""]+[])[+[]]+[...{}+[]][__+__- -~[]]+(!![]+[])[+[]]+[...{}+[]][-~[]]+(!![]+[])[-~[]];`;
+
+
     const range = (len, start) => Array.from(Array(len), (v, i) => i + start)
     const uniq = array => [...new Set(array)] // in array // out array
 
@@ -42,7 +45,7 @@
             if (input == myString) {
                 //使える文字なら => 置き換える
                 // console.log("置き換える場所")
-                output.push(input)
+                // output.push(input)
                 break;
 
             } else if('_end_' == myString) {
@@ -52,16 +55,17 @@
                 // console.log('でこーど')
                 //使えない文字なら => utf8に変換して、数字を記号化する必要がある
                 const pursedArr = [...myString.charCodeAt().toString(16)+[]]  
-                console.log(input,pursedArr)
+                // console.log(input,pursedArr)
                 if (pursedArr.length < 4) {
                     let returnArr = [...Array(4).fill(0)]
                     returnArr[2] = pursedArr[0]
                     returnArr[3] = pursedArr[1]
 
-                    console.log(returnArr)
+                    // console.log(returnArr)
                     
                 } else {
-//リスト内に内文字列だった場合に上記の処理を行いたい
+                    //リスト内に内文字列だった場合に上記の処理を行いたい
+                    console.error(pursedArr)
                 }
 
             }
@@ -69,6 +73,8 @@
         }
     }
 
+
+    console.log(baseBlackcode)
     console.log(inputs.map( x => replaceBlackCode(x)).join('+'))
     // console.log(output)
 
