544ed5bf7784323494efed4c4df56a4ccc503a3f
s17001 <s17001@std.it-college.ac.jp>
2019-05-28 13:59:16 +0900

    方針をきめた

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 (stdin => {
-    const baseBlackcode =`__=-~-~[-~[]];___=(!![]+[])[-~[]]+(!![]+[])[__]+(!![]+[])[+[]]+(!![]+[])[-~[-~[]]]+(!![]+[])[-~[]]+([][""]+[])[-~[]]+' \"'+'\\' +([][""]+[])[[]-[]] +'{' +(-~[]+[]) +(![]+[])[[]-[]] +__*__ +-~[] +-~[__] +'}' +'\"';_=[...{}+[]][__+__- -~[]]+[...{}+[]][-~[]]+([][""]+[])[-~[]]+(([]==[])+[])[__]+(-~[]/[]+[])[__+__]+(!![]+[])[-~[]]+([][""]+[])[+[]]+[...{}+[]][__+__- -~[]]+(!![]+[])[+[]]+[...{}+[]][-~[]]+(!![]+[])[-~[]];`;
-
+    const blackConstructor =`__=-~-~[-~[]];_=[...{}+[]][__+__- -~[]]+[...{}+[]][-~[]]+([][""]+[])[-~[]]+(([]==[])+[])[__]+(-~[]/[]+[])[__+__]+(!![]+[])[-~[]]+([][""]+[])[+[]]+[...{}+[]][__+__- -~[]]+(!![]+[])[+[]]+[...{}+[]][-~[]]+(!![]+[])[-~[]];`
 
     const range = (len, start) => Array.from(Array(len), (v, i) => i + start)
     const uniq = array => [...new Set(array)] // in array // out array
@@ -35,9 +34,9 @@
     // Main Procedure
 
     let output = [];
+    let frag = false;
 
     for (let input of inputs) {
-        let frag = false;
 
         for (let myString of canUseString) {
             // console.log(input,myString)
@@ -45,28 +44,27 @@
             if (input == myString) {
                 //使える文字なら => 置き換える
                 // console.log("置き換える場所")
-                // output.push(input)
+                output.push(replaceBlackCode(input))
+                frag = false
                 break;
 
             } else if('_end_' == myString) {
+            
+                    // console.log('でこーど')
+                    //使えない文字なら => utf8に変換して、数字を記号化する必要がある
+                    const pursedArr = [...input.charCodeAt().toString(16)+[]]  
 
-            } else {
+                    if (pursedArr.length < 4) {
+                        let returnArr = [...Array(4).fill(0)]
+                        returnArr[2] = pursedArr[0]
+                        returnArr[3] = pursedArr[1]
 
-                // console.log('でこーど')
-                //使えない文字なら => utf8に変換して、数字を記号化する必要がある
-                const pursedArr = [...myString.charCodeAt().toString(16)+[]]  
-                // console.log(input,pursedArr)
-                if (pursedArr.length < 4) {
-                    let returnArr = [...Array(4).fill(0)]
-                    returnArr[2] = pursedArr[0]
-                    returnArr[3] = pursedArr[1]
-
-                    // console.log(returnArr)
-                    
-                } else {
-                    //リスト内に内文字列だった場合に上記の処理を行いたい
-                    console.error(pursedArr)
-                }
+                        console.log(input,returnArr)
+                        //ここで形成した配列をさらにreplaceBlackCodeしてまとめたい
+                    } else {
+                        //リスト内に内文字列だった場合に上記の処理を行いたい
+                        console.error(pursedArr)
+                    }
 
             }
 
@@ -74,8 +72,8 @@
     }
 
 
-    console.log(baseBlackcode)
-    console.log(inputs.map( x => replaceBlackCode(x)).join('+'))
-    // console.log(output)
+    // console.log(blackConstructor)
+    // console.log(inputs.map( x => replaceBlackCode(x)).join('+'))
+    console.log(output)
 
   })(require('fs').readFileSync('/dev/stdin', 'utf8'));
